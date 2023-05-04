daffece5976f19eb003736231d1fbe69e365c473
Mumbi Muthiga <40498677+mumbimuthiga@users.noreply.github.com>
2023-05-04 10:30:26 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,44 @@
+// This file exports data in either excel,csv,pdf with the title as user has indicated
+//Script begins here
+var buttonCommon = {
+   init: function (dt, node, config) {
+     var table = dt.table().context[0].nTable;
+     if (table) config.title = $(table).data('export-title')
+   },
+   title: 'default title'
+ };
+ $.extend( $.fn.dataTable.defaults, {
+     "buttons": [
+        
+         $.extend( true, {}, buttonCommon, {
+             extend: 'pdfHtml5',
+             orientation: 'landscape',
+             exportOptions: {
+                 columns: ':visible'
+             }
+         } ),
+         {  
+            extend: 'copy'
+         },
+        ,
+        $.extend( true, {}, buttonCommon, {
+            extend: 'csv',
+         }),
+         $.extend( true, {}, buttonCommon, {
+            extend: 'excelHtml5',
+            exportOptions: {
+                columns: ':visible'
+            }
+        } ),
+     ]
+ } );
+ 
+ $('#myTable').DataTable( {
+   dom: 'Bfrtip',
+ });
+
+//Scripts ends here 
+
+
+// In the table add this part
+<table class="table table-bordered" id="myTable" width="100%" cellspacing="0" data-export-title="write the name of the file as you want it downloaded">
